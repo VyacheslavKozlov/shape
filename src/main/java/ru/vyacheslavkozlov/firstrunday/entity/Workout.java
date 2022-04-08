305ea5b31e62a0e9a4c54832165aa2d3f3864f57
vyacheslav kozlov <vyacheslavkozlof@gmail.com>
2022-04-08 22:01:27 +0300

    add id workout and annotation @entity

--- src/main/java/ru/vyacheslavkozlov/firstrunday/entity/Workout.java
+++ src/main/java/ru/vyacheslavkozlov/firstrunday/entity/Workout.java
@@ -1,4 +1,15 @@
 package ru.vyacheslavkozlov.firstrunday.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "workout")
 public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }
